895d8a6f9568c3a98277e93cb9793211f9f88bcb
Marco Bagnasco <admin@hecticmojo.com>
2016-04-11 17:20:35 +0100

    Adapted tests for added service/pagination

---
+++ test/spec/services/PageInfoSpec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('PageInfo service', function() {
+  var httpBackend, PageInfoService, data;
+  
+  beforeEach(function() {
+    module('iPlayerApp');
+  
+    inject(function($httpBackend, _PageInfo_) {
+      data = {'atoz_programmes': {"character":"a",
+                                  "count":74,
+                                  "page":1,
+                                  "per_page":20
+          }
+        };
+      httpBackend = $httpBackend
+      PageInfoService = _PageInfo_
+      httpBackend.whenGET('https://ibl.api.bbci.co.uk/ibl/v1/atoz/a/programmes?page=1').respond(200, data)
+      httpBackend.expectGET('views/main.html').respond(200, {})
+    })
+  })
+  
+  it('is not an empty object', function() {
+    expect(PageInfoService).toBeNonEmptyObject();
+  })
+  
+  it('has a method called #getList', function() {
+    expect(PageInfoService.getList()).toBeDefined();
+    expect(PageInfoService).toHaveMethod('getList');
+  })
+  
+  it('Retrieves info about the TotalItems in the current letter', function() {
+    PageInfoService.getList();
+    httpBackend.flush();
+    expect(list.count).toBe(74)
+  })
+  
+  it('Retrieves info about page number in the current letter', function() {
+    PageInfoService.getList();
+    httpBackend.flush();
+    expect(list.page).toBe(1)
+  })
+})
