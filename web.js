a8faa3424949793b12a5c1e2bdd7c0cca4fb9362
Marco Bagnasco <admin@hecticmojo.com>
2016-04-12 14:07:25 +0100

    Serving from app

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.listen(process.env.PORT || 3000);
