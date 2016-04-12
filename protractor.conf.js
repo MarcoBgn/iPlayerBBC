2350107e947e9d88e45e04f9cf975fcbfd61e32b
Marco Bagnasco <admin@hecticmojo.com>
2016-04-12 13:05:23 +0100

    Edited package.json

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,6 @@
 exports.config = {
-  // The address of a running selenium server.
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
-  // Spec patterns are relative to the location of this config.
   specs: [
     'test/e2e/*_spec.js'
   ],
@@ -13,9 +11,6 @@ exports.config = {
     'chromeOptions': {'args': ['--disable-extensions']}
   },
 
-
-  // A base URL for your application under test. Calls to protractor.get()
-  // with relative paths will be prepended with this.
   baseUrl: 'http://localhost:9000',
 
   jasmineNodeOpts: {
